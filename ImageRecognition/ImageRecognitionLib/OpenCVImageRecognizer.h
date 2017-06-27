7b7d0ea3f51769d0330a3455f040032bceeb43ab
Joe Schmitt <contact@jbob.io>
2017-06-27 14:07:09 -0700

    Add opencv dnn code to lib

---
+++ ImageRecognition/ImageRecognitionLib/OpenCVImageRecognizer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <opencv2/dnn.hpp>
+
+namespace ImageRecognitionLib
+{
+	public ref class OpenCVImageRecognizer sealed
+	{
+		cv::dnn::Net model;
+		std::vector<std::string> classNames;
+		OpenCVImageRecognizer(Platform::String^ modelFile, Platform::String^ classesFile);
+
+	public:
+		static Windows::Foundation::IAsyncOperation<OpenCVImageRecognizer^>^ OpenCVImageRecognizer::Create(Platform::String^ modelFile, Platform::String^ classesFile);
+		Windows::Foundation::IAsyncOperation<Platform::String^>^ RecognizeObjectAsync(const Platform::Array<byte>^ bytes);
+		uint32_t GetRequiredWidth();
+		uint32_t GetRequiredHeight();
+	};
+}
